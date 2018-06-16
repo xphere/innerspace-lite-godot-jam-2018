ff719550bd6bb1e8240da95afbe7675e1bdb0cd3
Berny Cantos <be@rny.cc>
2018-06-16 20:39:18 +0200

    ship spin acceleration

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,9 +1,24 @@
 extends Node2D
 
+const ROTATION_SPEED = 3
+const MAX_ROTATION_SPEED = 5
+const ROTATION_FRICTION = 0.8
+
+var rotation_intent = 0
+var rotation_velocity = 0
+
+
+func _input(delta):
+	rotation_intent = int(Input.is_action_pressed("rotate_right")) - int(Input.is_action_pressed("rotate_left"))
+
 
 func _physics_process(delta):
-	var motion_intent = int(Input.is_action_pressed("rotate_right")) - int(Input.is_action_pressed("rotate_left"))
-	rotation += motion_intent * delta
+	rotation_velocity += rotation_intent * delta * ROTATION_SPEED
+	rotation_velocity = clamp(rotation_velocity, -MAX_ROTATION_SPEED, MAX_ROTATION_SPEED)
+	if rotation_intent == 0:
+		rotation_velocity -= rotation_velocity * ROTATION_FRICTION * delta
+
+	rotation_degrees += rotation_velocity
 
 	if Input.is_action_pressed("fire"):
 		$Turret.fire()
