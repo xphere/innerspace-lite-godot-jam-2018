a4335efc0fea78988ac4a4310bedd8c164f2d13e
Berny Cantos <be@rny.cc>
2018-06-16 20:40:08 +0200

    add smoke to bullets

--- scenes/Bullet.gd
+++ scenes/Bullet.gd
@@ -2,13 +2,19 @@ extends Node2D
 
 const SPEED = 25
 
+var direction
+
+
+func _ready():
+	direction = Vector2(0, 1).rotated(rotation)
+
 
 func _physics_process(delta):
 	advance(delta)
 
 
 func advance(delta):
-	position -= Vector2(0, 1).rotated(rotation) * SPEED * delta
+	position -= direction * SPEED * delta
 
 
 func _on_VisibilityNotifier_screen_exited():
