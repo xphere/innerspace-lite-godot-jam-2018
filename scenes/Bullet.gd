4694a45afe1cbb7a8d02491d5154b55bbfff6546
Berny Cantos <be@rny.cc>
2018-06-17 12:47:09 +0200

    remove enemies gracefully

--- scenes/Bullet.gd
+++ scenes/Bullet.gd
@@ -12,20 +12,12 @@ func _ready():
 
 
 func _physics_process(delta):
-	advance(delta)
-
-
-func advance(delta):
 	position -= direction * SPEED * delta
-
-
-func _on_VisibilityNotifier_screen_exited():
-	queue_free()
 
 
 func _on_Bullet_area_entered(area):
 	var explosion = explosion_scene.instance()
-	explosion.global_position = global_position
+	explosion.global_position = area.global_position
 	explosion.set_as_toplevel(true)
 	get_parent().add_child(explosion)
 	queue_free()
--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -24,3 +24,19 @@ func _physics_process(delta):
 	direction = (direction + ANGULAR_FRICTION * delta * (track_direction - direction)).normalized()
 	speed = min(speed + SPEED * delta, MAX_SPEED)
 	position += direction * speed * delta
+
+
+func destroy():
+	self.set_physics_process(false)
+	$Sprite.queue_free()
+	$Enemy.queue_free()
+	$Tween.interpolate_property(self, 'modulate', modulate, Color(0, 0, 0, 0), 1.0, Tween.TRANS_QUAD, Tween.EASE_IN)
+	$Tween.start()
+
+
+func _on_Enemy_area_entered(area):
+	destroy()
+
+
+func _on_Tween_tween_completed(object, key):
+	queue_free()
